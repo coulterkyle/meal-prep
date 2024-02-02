1bc1b6ad141e645b952adb656a39df393bb7f5aa
kyle coulter <152663440+coulterkyle@users.noreply.github.com>
2024-02-02 11:36:45 -0500

    got search function working

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes');
+const recipeRoutes = require('./recipe-routes');
 
 router.use('/users', userRoutes);
+router.use('/recipes', recipeRoutes);
 
 
 
---
+++ models/Recipe.js
@@ -0,0 +1,54 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Recipe extends Model { }
+
+Recipe.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    recipe_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    recipe_thumbnail: {
+      type: DataTypes.STRING,
+      //   allowNull: false,
+    },
+    recipe_ingredients: {
+      type: DataTypes.JSON,
+      //   allowNull: false,
+    },
+    recipe_calories: {
+      type: DataTypes.INTEGER,
+      //   allowNull: false,
+    },
+    recipe_instructions: {
+      type: DataTypes.INTEGER,
+      //   allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+      allowNull: false,
+      onDelete: 'CASCADE'
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'recipe',
+  }
+);
+
+module.exports = Recipe;
+
