91c48f21f03b20e4334032e8465aab9fadab657d
CodeClass0 <chadandari@outlook.com>
2024-02-02 11:14:09 -0500

    Roughed in navigation routes

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,15 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes');
+const dashRoutes = require('./dash-routes');
+const recipeRoutes = require('./recipe-routes');
+const dayRoutes = require('./day-routes');
+const weekRoutes = require('./week-routes');
 
+router.use('/recipe', recipeRoutes);
+router.use('/day', dayRoutes);
+router.use('/week', weekRoutes);
+router.use('/dash', dashRoutes);
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
 
