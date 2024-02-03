476d25b0ee69118b307ad3720542d3f19eb70f45
CodeClass0 <chadandari@outlook.com>
2024-02-03 12:43:42 -0500

    Minor tweak to package.json and index.js controller

--- controllers/index.js
+++ controllers/index.js
@@ -3,11 +3,9 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes');
 const dashRoutes = require('./dash-routes');
-const recipeRoutes = require('./recipe-routes');
 const dayRoutes = require('./day-routes');
 const weekRoutes = require('./week-routes');
 
-router.use('/recipe', recipeRoutes);
 router.use('/day', dayRoutes);
 router.use('/week', weekRoutes);
 router.use('/dash', dashRoutes);
