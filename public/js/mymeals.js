115d8465a8b91f7ebea55283ed4e886b44f07e13
CodeClass0 <chadandari@outlook.com>
2024-02-03 15:05:39 -0500

    Remove from list now refreshing

--- public/js/mymeals.js
+++ public/js/mymeals.js
@@ -2,12 +2,16 @@ console.log('linked')
 
 async function deleteMeals(id){
     if (id){
-        deleteRequest = await fetch(`http://localhost:3001/api/recipes/${id}`, {
+        const deleteRequest = await fetch(`http://localhost:3001/api/recipes/${id}`, {
             method: "DELETE",
             headers: { 'Content-Type': 'application/json' },
         });
-    }
-    if (response.ok){
-        
+        if (deleteRequest.ok){
+            const rePopulate = await fetch('/mymeals', {
+                method: "GET",
+                headers: { 'Content-Type': 'application/json' },
+            })
+        }
+        document.location.assign('/mymeals');
     }
 }
