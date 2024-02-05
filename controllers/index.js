0897e269d22c75746b3944affdedd82c5cf8fa74
kyle coulter <152663440+coulterkyle@users.noreply.github.com>
2024-02-05 15:09:00 -0500

    fixed nav active feature

--- controllers/index.js
+++ controllers/index.js
@@ -2,13 +2,7 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes');
-const dashRoutes = require('./dash-routes');
-const dayRoutes = require('./day-routes');
-const weekRoutes = require('./week-routes');
 
-router.use('/day', dayRoutes);
-router.use('/week', weekRoutes);
-router.use('/dash', dashRoutes);
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
 
